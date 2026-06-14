cbae3e552a299c9cfc64af15fce7fa8014e4710b
Alexc0dex <mirandavivancoalexander@gmail.com>
2026-06-14 00:20:22 -0500

    feat: layout navbar added

--- src/app/layouts/app-layout/app-layout.component.ts
+++ src/app/layouts/app-layout/app-layout.component.ts
@@ -1,16 +1,26 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-app-layout.component',
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    RouterLinkActive,
+    RouterLink,
+  ],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.css',
 })
 export class AppLayoutComponent {
-
   private authService = inject(AuthService);
   private router = inject(Router);
 
