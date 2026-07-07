15c35bcbcd2ff859ef14b392b1339d3c75e7b5b6
MayYTa <101844433+MayYTa@users.noreply.github.com>
2026-07-07 17:41:23 -0500

    feature: se implemento la pestaña de configuracion de perfil con pantallas para metricas del cuerpo y informacion general y otra para cambiar contraseña (#12)
    ta bien 👍

---
+++ src/app/pages/profile-settings/profile-settings.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { ProfileSettings } from './profile-settings';
+
+export const routes: Routes = [
+  { 
+path: '', component: ProfileSettings },
+];
